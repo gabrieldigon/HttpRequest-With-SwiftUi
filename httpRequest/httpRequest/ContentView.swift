f225afdeffd329ef3636e53d54037ca6c1f16440
gabrieldigon <gabrieldiasgonca@gmail.com>
2024-02-15 21:27:19 -0400

    FirstCommit

---
+++ httpRequest/httpRequest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  httpRequest
+//
+//  Created by Gabriel Dias Goncalves on 15/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ httpRequest/httpRequest/httpRequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  httpRequestApp.swift
+//  httpRequest
+//
+//  Created by Gabriel Dias Goncalves on 15/02/24.
+//
+
+import SwiftUI
+
+@main
+struct httpRequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
